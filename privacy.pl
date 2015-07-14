b6c6d3f65f6dfa5a8234f3a6f00a06332d8c6bdd
jtylers <jtylers@byu.edu>
2015-07-14 17:15:23 -0600

    Created privacy script

---
+++ privacy.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl
+#Created By Tyler S Johnson
+#July 10, 2015
+#For best performance, add an alias to your ~/.bashrc that runs this script
+
+#hide output
+`stty -echo`;
+
+#clear terminal
+print ` clear`;
+my $cmd = "hidden";
+my $option = shift;
+
+# No footprints in History
+if ($option){#Hide ALL output
+        while ($cmd ne "q\n"){
+                $cmd = <STDIN>;
+                ` $cmd`;
+        }
+} else {#Hide only typed output
+        while ($cmd ne "q\n"){
+        $cmd = <STDIN>;
+        system (" $cmd");
+        }
+}
+
+#reset terminal
+`stty echo`;
+print `clear`;
