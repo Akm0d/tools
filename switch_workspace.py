2d432f5ee165f050761305c2ab1b87af4ef71878
Tyler Johnson <jtylers@byu.edu>
2016-10-05 14:08:40 -0600

    Update switch_workspace.py

--- switch_workspace.py
+++ switch_workspace.py
@@ -10,5 +10,9 @@ if sys.argv[1].upper() == "RIGHT":
     os.system("xdotool keydown ctrl alt Right;xdotool keyup ctrl alt Right")
 elif sys.argv[1].upper() == "LEFT":
     os.system("xdotool keydown ctrl alt Left;xdotool keyup ctrl alt Left")
+if sys.argv[1].upper() == "MOVE_RIGHT":
+    os.system("xdotool keydown shift ctrl alt Right;xdotool keyup shift ctrl alt Right")
+elif sys.argv[1].upper() == "MOVE_LEFT":
+    os.system("xdotool keydown shift ctrl alt Left;xdotool keyup shift ctrl alt Left")
 else:
     print("Invalid Argument \"",sys.argv[1],"\"")
